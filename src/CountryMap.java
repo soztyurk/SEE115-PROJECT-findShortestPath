47669239db18612ecc109a60b78e5cc5ab6c6893
soztyurk <soztyurk@gmail.com>
2024-12-17 15:45:13 +0300

    It stores paths and distances between cities.

--- src/CountryMap.java
+++ src/CountryMap.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class CountryMap {
+public class CountryMap {
+    private City[] cities;
+    private int[][] adjacencyMatrix;
+
+    public CountryMap(City[] cities, int[][] adjacencyMatrix) {
+        this.cities = cities;
+        this.adjacencyMatrix = adjacencyMatrix;
+    }
+
+    public City[] getCities() {
+        return cities;
+    }
+
+    public int[][] getAdjacencyMatrix() {
+        return adjacencyMatrix;
+    }
 }
